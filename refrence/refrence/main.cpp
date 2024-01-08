10c24a51aeb24d2ebd550006050d15072af2e02f
Nisha Ramprasath <115043583+nisharampra@users.noreply.github.com>
2024-01-08 22:59:16 +0800

    Add files via upload

---
+++ refrence/refrence/main.cpp
@@ -0,0 +1,24 @@
+//
+//  main.cpp
+//  refrence
+//
+//  Created by Nisha Ramprasath on 1/11/23.
+//
+
+#include <iostream>
+using namespace std;
+    // insert code here...
+    int main(){
+        
+        
+        int x=10;
+        int &y=x;
+        
+        int b=20;
+        cout<<y<<endl;
+        y++;
+        x++;
+        
+        
+        
+    }
